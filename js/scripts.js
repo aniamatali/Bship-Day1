e962b1a2b92700180390f9eaf10932f8a24da95c
alvin niamatali <aniamatali@icloud.com>
2017-10-02 06:32:55 -0700

    replicating what i did for a1-a6 hit and miss stuff

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,32 @@
 $(document).ready(function() {
 
 var a1 = [];
+var a2 = [];
+var a3 = [];
+var a4 = [];
+var a5 = [];
+var a6 = [];
 
+var b1 = [];
+var b2 = [];
+var b3 = [];
+var b4 = [];
+var b5 = [];
+var b6 = [];
+
+var c1 = [];
+var c2 = [];
+var c3 = [];
+var c4 = [];
+var c5 = [];
+var c6 = [];
+
+var d1 = [];
+var d2 = [];
+var d3 = [];
+var d4 = [];
+var d5 = [];
+var d6 = [];
 
   $("#a1").click(function() {
     this.value = "a1";
@@ -10,6 +35,191 @@ var a1 = [];
     if (a1.length === 1) {
     alert("miss");
   } else if (a1.length === 2) {
+    alert("hit");
+  }
+  });
+
+  $("#a2").click(function() {
+    this.value = "a2";
+    a2.push(+1);
+    console.log(a1);
+    if (a2.length === 1) {
+    alert("miss");
+  } else if (a2.length === 2) {
+    alert("hit");
+  }
+  });
+
+  $("#a3").click(function() {
+    this.value = "a1";
+    a3.push(+1);
+    console.log(a1);
+    if (a3.length === 1) {
+    alert("miss");
+  } else if (a3.length === 2) {
+    alert("hit");
+  }
+  });
+
+  $("#a4").click(function() {
+    this.value = "a1";
+    a4.push(+1);
+    console.log(a1);
+    if (a4.length === 1) {
+    alert("miss");
+  } else if (a4.length === 2) {
+    alert("hit");
+  }
+  });
+
+  $("#a5").click(function() {
+    this.value = "a1";
+    a1.push(+1);
+    console.log(a1);
+    if (a5.length === 1) {
+    alert("miss");
+  } else if (a5.length === 2) {
+    alert("hit");
+  }
+  });
+
+  $("#a6").click(function() {
+    this.value = "a1";
+    a1.push(+1);
+    console.log(a1);
+    if (a6.length === 1) {
+    alert("miss");
+  } else if (a6.length === 2) {
+    alert("hit");
+  }
+  });
+
+  $("#b1").click(function() {
+    this.value = "a1";
+    b1.push(+1);
+    console.log(a1);
+    if (b1.length === 1) {
+    alert("miss");
+  } else if (b1.length === 2) {
+    alert("hit");
+  }
+  });
+
+  $("#b2").click(function() {
+    this.value = "a1";
+    b2.push(+1);
+    console.log(a1);
+    if (b1.length === 1) {
+    alert("miss");
+  } else if (b1.length === 2) {
+    alert("hit");
+  }
+  });
+
+  $("#b3").click(function() {
+    this.value = "a1";
+    b3.push(+1);
+    if (b3.length === 1) {
+    alert("miss");
+  } else if (b3.length === 2) {
+    alert("hit");
+  }
+  });
+
+  $("#b4").click(function() {
+    this.value = "a1";
+    b4.push(+1);
+    console.log(a1);
+    if (b4.length === 1) {
+    alert("miss");
+  } else if (b4.length === 2) {
+    alert("hit");
+  }
+  });
+
+  $("#b5").click(function() {
+    this.value = "a1";
+    b5.push(+1);
+    console.log(a1);
+    if (b5.length === 1) {
+    alert("miss");
+  } else if (b5.length === 2) {
+    alert("hit");
+  }
+  });
+
+  $("#b6").click(function() {
+    this.value = "a1";
+    b6.push(+1);
+    console.log(a1);
+    if (b6.length === 1) {
+    alert("miss");
+  } else if (b6.length === 2) {
+    alert("hit");
+  }
+  });
+
+  $("#c1").click(function() {
+    this.value = "a1";
+    c1.push(+1);
+    console.log(a1);
+    if (c1.length === 1) {
+    alert("miss");
+  } else if (c1.length === 2) {
+    alert("hit");
+  }
+  });
+
+  $("#b2").click(function() {
+    this.value = "a1";
+    b2.push(+1);
+    console.log(a1);
+    if (b1.length === 1) {
+    alert("miss");
+  } else if (b1.length === 2) {
+    alert("hit");
+  }
+  });
+
+  $("#b3").click(function() {
+    this.value = "a1";
+    b3.push(+1);
+    if (b3.length === 1) {
+    alert("miss");
+  } else if (b3.length === 2) {
+    alert("hit");
+  }
+  });
+
+  $("#b4").click(function() {
+    this.value = "a1";
+    b4.push(+1);
+    console.log(a1);
+    if (b4.length === 1) {
+    alert("miss");
+  } else if (b4.length === 2) {
+    alert("hit");
+  }
+  });
+
+  $("#b5").click(function() {
+    this.value = "a1";
+    b5.push(+1);
+    console.log(a1);
+    if (b5.length === 1) {
+    alert("miss");
+  } else if (b5.length === 2) {
+    alert("hit");
+  }
+  });
+
+  $("#b6").click(function() {
+    this.value = "a1";
+    b6.push(+1);
+    console.log(a1);
+    if (b6.length === 1) {
+    alert("miss");
+  } else if (b6.length === 2) {
     alert("hit");
   }
   });
