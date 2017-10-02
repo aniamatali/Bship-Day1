75bd495e1966af4ad76d646e1e0ad348ccbf172e
alvin niamatali <aniamatali@icloud.com>
2017-10-02 13:16:19 -0700

    removed checkboxes from the combined grids for the game play

--- js/scripts.js
+++ js/scripts.js
@@ -70,6 +70,13 @@ $(document).ready(function() {
     });
 
 
+$("#a1, #a2, #a3, #a4, #a5, #a6, #a7, #a8, #a9, #a10, #a11, #a12, #a13, #a14, #a15, #a16, #a17, #a18, #a19, #a20, #a21, #a22, #a23, #a24, #a25, #a26, #a27, #a28, #a28, #a29, #a30, #a31, #a32, #a33, #a34, #a35, #a36").click(function() {
+  var smtg = this.id;
+})
+
+$("#b1, #b2, #b3, #b4, #b5, #b6, #b7, #b8, #b9, #b10, #b11, #b12, #b13, #b14, #b15, #b16, #b17, #b18, #b19, #b20, #b21, #b22, #b23, #b24, #b25, #b26, #b27, #b28, #b28, #b29, #b30, #b31, #b32, #b33, #b34, #b35, #b36").click(function() {
+  var smtg2 = this.id;
+})
 
 // $("").submit(function(event)){
 //   event.preventDefault();
